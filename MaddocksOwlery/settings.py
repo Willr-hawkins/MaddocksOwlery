26fb3e9dedc5837675431800bb1647e9ba703a43
William Hawkins <Hawkinswill02@gmail.com>
2025-05-28 13:55:53 +0000

    Fixed deployment issues so set debug back to false

--- MaddocksOwlery/settings.py
+++ MaddocksOwlery/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['maddocksowlery.onrender.com', '8000-willrhawkin-maddocksowl-j3tnzmy693f.ws-eu120.gitpod.io']
 
