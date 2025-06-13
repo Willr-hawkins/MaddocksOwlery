e33b8345403ca95b4bbb8f0d988625db22885381
William Hawkins <Hawkinswill02@gmail.com>
2025-06-13 09:31:42 +0000

    Updated footer content along with adding both a privacy policy and a cookies policy in the footer

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('cookie-policy/', views.cookie_policy, name='cookie_policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home_page.html')
+
+def privacy_policy(request):
+    return render(request, '../templates/privacy_policy.html')
+
+def cookie_policy(request):
+    return render(request, '../templates/cookies_policy.html')
