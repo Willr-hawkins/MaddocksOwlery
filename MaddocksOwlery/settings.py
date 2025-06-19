7f59622a6c2621250d219edd59e79a1d1389bd02
William Hawkins <Hawkinswill02@gmail.com>
2025-06-19 12:28:32 +0000

    Added newsletter signup form to the footer along with add styles for footer content.

--- MaddocksOwlery/settings.py
+++ MaddocksOwlery/settings.py
@@ -165,6 +165,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
