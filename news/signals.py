6408f912f1ef255d7da7c2c9b6c73e3c08666d33
William Hawkins <Hawkinswill02@gmail.com>
2025-06-25 16:22:28 +0000

    Added essectial import to signals.py

--- news/signals.py
+++ news/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from .models import NewsUpdate
 
 @receiver(post_save, sender=NewsUpdate)
